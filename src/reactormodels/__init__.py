878a42ec23eb0f797cfde718fea987e976317891
mohammedfaran <faranmohammed9223@gmail.com>
2026-06-09 17:39:06 -0400

    Keep only isotherm changes

--- src/reactormodels/__init__.py
+++ src/reactormodels/__init__.py
@@ -2,7 +2,6 @@ from . import models
 from . import numerics
 from . import postprocess
 from .column_data import Column
-from .breakthrough_class import breakthrough_data
 from .Isotherm_class import fit_isotherm_parameters
 
 __all__ = [
@@ -10,6 +9,5 @@ __all__ = [
     "numerics",
     "postprocess",
     "Column",
-    "breakthrough_data",
     "Isotherm_class",
 ]
